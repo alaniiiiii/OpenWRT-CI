ac7eba55957d9630f81d93a87346c9682ea4be48
alaniiiiii <104252234+alaniiiiii@users.noreply.github.com>
2025-10-25 17:33:13 +0800

    Add files via upload

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#增加软件源
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
